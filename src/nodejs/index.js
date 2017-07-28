52ab0b3df5a3215ae14956cd9498de957bd9fd4a
Albert Hilazo <albhilazo@gmail.com>
2017-07-28 16:56:04 +0200

    Refactor server initialization logic

--- src/nodejs/index.js
+++ src/nodejs/index.js
@@ -1,1 +1,10 @@
-require('./app/index')
+const app = require('./app')
+const port = process.env.PORT || 3000
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err
+  }
+
+  console.log(`server is listening on port ${port}`)
+})
