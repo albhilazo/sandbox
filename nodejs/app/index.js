bd65b3e2e7cc90385ce5b2cca6445a5eef4fb850
Albert Hilazo <albhilazo@gmail.com>
2017-08-12 18:38:19 +0200

    Refactor server initialization logic

--- nodejs/app/index.js
+++ nodejs/app/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const dice = require('./dice')
 
 const app = express()
-const port = 3000
 
 app.get('/', (request, response) => {
   const sides = request.query.sides || 6
@@ -11,10 +10,4 @@ app.get('/', (request, response) => {
   response.send(`Rolled ${sides} and got: ${result}`)
 })
 
-app.listen(port, (err) => {
-  if (err) {
-    return console.log(`Error: ${err}`)
-  }
-
-  console.log(`server is listening on port ${port}`)
-})
+module.exports = app
